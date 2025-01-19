ebb23bab82d80217378a5f298b62ab49196406f2
othmanebelkarda <belkardaothmane05@gmail.com>
2025-01-19 19:56:32 +0100

    cors

--- index.js
+++ index.js
@@ -1,80 +1,66 @@
 const express = require("express");
+const cors = require("cors"); // Import cors package
 
 const app = express();
-
 const mongoose = require("mongoose");
-
 const Article = require("./models/Article.js");
 
-//mongodb+srv://belkardaothmane05:<password>@cluster0.1l2p35k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;
-
+// MongoDB connection
 mongoose.connect("mongodb+srv://belkardaothmane05:Othmane&2005@cluster0.1l2p35k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>{
-console.log("connected succesfully");
-
-}).catch((error)=>{
-    console.log("error with connecting with data base",error);
+.then(() => {
+    console.log("connected successfully");
+}).catch((error) => {
+    console.log("error with connecting to the database", error);
 });
 
+// Use CORS middleware globally
+app.use(cors({
+    origin: '*', // Allow only this origin (adjust if necessary)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
 
-app.listen(3000, ()=>{
-    console.log("I'm listening in port 3000")
+app.listen(3000, () => {
+    console.log("I'm listening on port 3000");
 });
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 
 app.use(express.json());
 
-app.post("/articles", async(req,res)=>{
+app.post("/articles", async (req, res) => {
     const newArticle = new Article();
-    const ParentLastName = req.body.ParentLastName ;
-    const ParentFirstName = req.body.ParentFirstName ;
-    const StudentLastName = req.body.StudentLastName;
-    const StudentFirstName = req.body.StudentFirstName;
-    const Age = req.body.Age;
-    const Niveau = req.body.Niveau;
-    const Email = req.body.Email;
-    const Numero = req.body.Numero;
- 
-    newArticle.ParentLastName=ParentLastName;
-    newArticle.ParentFirstName=ParentFirstName;
-    newArticle.StudentLastName=StudentLastName;
-    newArticle.StudentFirstName=StudentFirstName;
-    newArticle.Age=Age;
-    newArticle.Niveau=Niveau;
-    newArticle.Email=Email;
-    newArticle.Numero=Numero;
-    await newArticle.save()
-    res.json(newArticle)
+    const { ParentLastName, ParentFirstName, StudentLastName, StudentFirstName, Age, Niveau, Email, Numero } = req.body;
+
+    newArticle.ParentLastName = ParentLastName;
+    newArticle.ParentFirstName = ParentFirstName;
+    newArticle.StudentLastName = StudentLastName;
+    newArticle.StudentFirstName = StudentFirstName;
+    newArticle.Age = Age;
+    newArticle.Niveau = Niveau;
+    newArticle.Email = Email;
+    newArticle.Numero = Numero;
+
+    await newArticle.save();
+    res.json(newArticle);
 });
-app.get("/article",async(req,res)=>{
-  try{ 
-   //   const article = await Article.findById(id);
-   const article = await Article.find();
-    res.json(article);
-    console.log("the article is",article);
-    } 
-  catch(error){
-        console.log("error");
-        return res.send("error")
+
+app.get("/article", async (req, res) => {
+    try {
+        const articles = await Article.find();
+        res.json(articles);
+        console.log("The articles are:", articles);
+    } catch (error) {
+        console.log("Error");
+        return res.send("Error");
     }
-})
+});
 
-
-app.delete("/articles/:articleId",async(req,res)=>{
-   const id = req.params.articleId;
-    try{
+app.delete("/articles/:articleId", async (req, res) => {
+    const id = req.params.articleId;
+    try {
         const article = await Article.findByIdAndDelete(id);
         res.json(article);
+    } catch (error) {
+        console.log("Error");
+        return res.send("Error");
     }
-    catch(error){
-        console.log("error");
-        return res.send("error")
-    }
-})
+});
